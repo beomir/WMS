755efe6f4a0406063dfa92a2d661f03811223d95
Andrzej <beomir89@gmail.com>
2020-12-04 11:13:55 +0100

    fix

--- src/main/java/pl/coderslab/cls_wms_app/controller/StartController.java
+++ src/main/java/pl/coderslab/cls_wms_app/controller/StartController.java
@@ -4,10 +4,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.coderslab.cls_wms_app.entity.Users;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
