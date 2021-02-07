d875423eed4ff1870d4ffd9e8e2b0da3dcdd600a
Andrzej Leszka <beomir89@gmail.com>
2021-02-07 20:56:42 +0100

    hot fix

--- src/main/java/pl/coderslab/cls_wms_app/ClsWmsAppApplication.java
+++ src/main/java/pl/coderslab/cls_wms_app/ClsWmsAppApplication.java
@@ -2,11 +2,11 @@ package pl.coderslab.cls_wms_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ClsWmsAppApplication {
-
-
 
     public static void main(String[] args) {
         SpringApplication.run(ClsWmsAppApplication.class, args);
