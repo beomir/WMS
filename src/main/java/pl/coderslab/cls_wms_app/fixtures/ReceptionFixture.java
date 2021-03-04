604d522ae4038d09224e9161fee0bb01860f831c
Andrzej Leszka <beomir89@gmail.com>
2021-03-04 11:50:15 +0100

    heroku upload

--- src/main/java/pl/coderslab/cls_wms_app/fixtures/ReceptionFixture.java
+++ src/main/java/pl/coderslab/cls_wms_app/fixtures/ReceptionFixture.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Random;
 
 @Component
-@Profile("local")
+//@Profile("local")
 public class ReceptionFixture {
 
     private ReceptionService receptionService;
