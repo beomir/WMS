63aca9500097712348a8e759c9c1e5545d181770
Andrzej Leszka <beomir89@gmail.com>
2021-03-25 14:54:28 +0100

    authentication success handler for scanner user role

---
+++ src/main/java/pl/coderslab/cls_wms_app/app/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,40 @@
+package pl.coderslab.cls_wms_app.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import pl.coderslab.cls_wms_app.service.userSettings.UsersService;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@Configuration
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private UsersService usersService;
+
+    @Autowired
+    public void setUsersService(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
+        if (roles.contains("ROLE_SCANNER")) {
+            httpServletResponse.sendRedirect("/scanner");
+        } else {
+            httpServletResponse.sendRedirect("/warehouse");
+        }
+    }
+}
--- src/main/java/pl/coderslab/cls_wms_app/app/SecurityConfig.java
+++ src/main/java/pl/coderslab/cls_wms_app/app/SecurityConfig.java
@@ -1,36 +1,45 @@
 package pl.coderslab.cls_wms_app.app;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import pl.coderslab.cls_wms_app.temporaryObjects.CustomerUserDetailsService;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomerUserDetailsService customerUserDetailsService;
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    public SecurityConfig(CustomerUserDetailsService customerUserDetailsService, AuthenticationSuccessHandler authenticationSuccessHandler) {
+        this.customerUserDetailsService = customerUserDetailsService;
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    // Konfiguracja procesu uwierzytelniania użytkownika (potocznie: logowanie)
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(customerUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
-    // Konfiguracja całości zasad bezpieczeństwa, np. autoryzacji ścieżek (tzw. endpointów)
-    // czy procesu logowania, czy unieważniania sesji, wylogowywania, zapamiętywania użytkownika, itd.
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -58,7 +67,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .usernameParameter("username") // username
                 .passwordParameter("password") // password
-                .defaultSuccessUrl("/warehouse")
+                .successHandler(authenticationSuccessHandler)
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
