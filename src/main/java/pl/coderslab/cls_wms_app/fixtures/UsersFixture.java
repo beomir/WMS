16648f85d297e536c54d7d4f30bbe75a317d7ef4
Andrzej Leszka <beomir89@gmail.com>
2021-04-09 13:35:34 +0200

    h1

--- src/main/java/pl/coderslab/cls_wms_app/fixtures/UsersFixture.java
+++ src/main/java/pl/coderslab/cls_wms_app/fixtures/UsersFixture.java
@@ -70,6 +70,8 @@ public class UsersFixture {
         Users users15 = usersList.get(14);
         Users users16 = usersList.get(15);
         Users users17 = usersList.get(16);
+        Users users18 = usersList.get(17);
+        Users users19 = usersList.get(18);
 //
         users1.setUsersRoles(usersRolesList.get(0));
         users2.setUsersRoles(usersRolesList.get(1));
@@ -88,6 +90,8 @@ public class UsersFixture {
         users15.setUsersRoles(usersRolesList.get(4));
         users16.setUsersRoles(usersRolesList.get(0));
         users17.setUsersRoles(usersRolesList.get(5));
+        users18.setUsersRoles(usersRolesList.get(0));
+        users19.setUsersRoles(usersRolesList.get(5));
 //
         usersService.addWithoutCodePass(users1);
         usersService.addWithoutCodePass(users2);
@@ -106,5 +110,7 @@ public class UsersFixture {
         usersService.addWithoutCodePass(users15);
         usersService.addWithoutCodePass(users16);
         usersService.addWithoutCodePass(users17);
+        usersService.addWithoutCodePass(users18);
+        usersService.addWithoutCodePass(users19);
     }
 }
--- src/main/resources/static/js/logo.js
+++ src/main/resources/static/js/logo.js
@@ -5,7 +5,7 @@ function logo(){
     document.getElementById("div1").innerHTML = "CLS";
 
 
-    if(window.location.pathname != "/index"){
+    if(window.location.pathname != "/index" || window.location.pathname != "/cls-wms.herokuapp.com"){
         const hamburger = document.querySelector('.hamburger');
         const nav = document.querySelector('.navigation');
 
